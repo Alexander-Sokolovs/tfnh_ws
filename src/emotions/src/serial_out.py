b29bcd753137bfcc3eaed3435365869234f72872
Alexander Sokolovs <alsok20@student.sdu.dk>
2022-04-21 21:55:13 +0200

    testing serial connection

--- src/emotions/src/serial_out.py
+++ src/emotions/src/serial_out.py
@@ -9,23 +9,21 @@ class serial_out_node:
     def __init__(self):
         print("Starting serial_out")
 
-        self.ser = serial.Serial('/dev/ttyS0')
+        self.ser = serial.Serial('/dev/ttyS0', baudrate=9600)
 
         self.rate = rospy.Rate(15)
 
         self.prio_listener = rospy.Subscriber("/prio/motor_spd", motor_spd, self.serial_out_cb)
 
-        self.ser.write(b'hello world')
-
     def serial_out_cb(self, msg):
         #print("Insert code for serial sending")
-        self.ser.write(msg)  
+        message = str(msg.m1) +','+str(msg.m2)
+
+        self.ser.write(str.encode(message))
 
 if __name__ == "__main__":
     rospy.init_node("serial_out")
 
     s_out = serial_out_node()
     rospy.spin()
-
     s_out.ser.close()
-    #s_out.run()
