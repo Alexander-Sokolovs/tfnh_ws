b23530eb86a04ca4f51b0c8f575c53fd01f56f21
Alexander Sokolovs <alsok20@student.sdu.dk>
2022-04-30 23:34:08 +0200

    minor changes

--- src/emotions/src/us_node.py
+++ src/emotions/src/us_node.py
@@ -18,7 +18,7 @@ class ultrasound_node:
         self.trig_pins = [17]
         for i in range(len(self.echo_pins)):
             self.sensors.append(DistanceSensor(echo=self.echo_pins[i], trigger=self.trig_pins[i]))
-            self.us_pubs.append(rospy.Publisher(str("/us/distances_"+i), Int16, queue_size=1))
+            self.us_pubs.append(rospy.Publisher(str("/us/distances_"+str(i)), Int16, queue_size=1))
 
     def run(self):
         while not rospy.is_shutdown():
