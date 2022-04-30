f65c9b90c1823b5bb5851051b18b94db522036ce
Alexander Sokolovs <alsok20@student.sdu.dk>
2022-04-30 23:37:24 +0200

    minor changes

--- src/emotions/src/us_node.py
+++ src/emotions/src/us_node.py
@@ -23,7 +23,7 @@ class ultrasound_node:
     def run(self):
         while not rospy.is_shutdown():
             for i in range(len(self.echo_pins)):
-                self.us_pubs[i].publish(self.sensors[i].distance*100)
+                self.us_pubs[i].publish(int(self.sensors[i].distance*100))
                 sleep(0.1)
             self.rate.sleep()
 
